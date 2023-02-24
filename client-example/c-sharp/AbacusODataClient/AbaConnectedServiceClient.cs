92104f3f1f3f7861ecd9267b4ad2ac40a29f1bb9
Scraylex <lukas.volk2014@gmail.com>
2023-02-24 08:15:57 +0100

    Rename, remove unused imports and add some explainatory comments to AbaClient

---
+++ client-example/c-sharp/AbacusODataClient/AbaConnectedServiceClient.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+namespace AbacusODataClient
+{
+    /**
+     * This class uses the microsoft connected odata service. To use it as is it requires following the tutorial mentioned in the README
+     */
+
+    public class AbaConnectedServiceClient
+    {
+        private readonly static string SERVICE_URL = "https://entity-api1-1.demo.abacus.ch/entity/v1/mandants/7777";
+        private readonly static string AUTH_URL = "https://entity-api1-1.demo.abacus.ch/oauth/oauth2/v1/token";
+        private readonly TokenCache _tokenCache;
+       // private readonly Container _context;
+
+        /**
+         * Here we add a request interceptor to handle our Client Credential Code Grant while accessing the Abacus API
+         */
+
+        public AbaConnectedServiceClient()
+        {
+            //_tokenCache = new TokenCache(new Authenticator(AUTH_URL));
+            //_context = new Container(new Uri(SERVICE_URL));
+            //_context.BuildingRequest += (sender, eventArgs) =>
+            //{
+            //    eventArgs.Headers.Add("Authorization", CreateAuthHeaderValue());
+            //};
+        }
+
+        private string CreateAuthHeaderValue()
+        {
+            return "Bearer " + _tokenCache.GetAccessToken();
+        }
+
+        /**
+         * This is an example function to query the Subjects async using the connected service
+         */
+
+        //public async Task<IEnumerable<Subject>> GetSubjectsAsync()
+        //{
+        //    return await _context.Subjects.ExecuteAsync();
+        //}
+    }
+}
